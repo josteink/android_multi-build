7e173bf399e94a2d20c3b9772a82c2e1cda78b0b
Jostein Kjønigsen <jostein@kjonigsen.net>
2013-02-15 09:59:50 +0100

    Cleaned up build config.

--- run_all_builds.sh
+++ run_all_builds.sh
@@ -3,14 +3,15 @@
 # script-folder, without trailing /
 BUILDER_BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# local build-root
+# build runner
+DO_BUILD=$BUILDER_BASE/execute_builds.sh
+
+# CONFIGURE: local build-root
 ANDROID_BASE=/mnt/build/android
 
-# remote deploy-root, relative or absolute path once logged in.
+# CONFIGURE: remote deploy-root, relative or absolute path once logged in.
 # does not refer to a internet DNS name or anything like that.
 DEPLOY_BASE=./nexfiles.kjonigsen.net
-
-DO_BUILD=$BUILDER_BASE/execute_builds.sh
 
 #
 # FORMAT:
@@ -29,6 +30,3 @@ DO_BUILD=$BUILDER_BASE/execute_builds.sh
 $DO_BUILD maguro $ANDROID_BASE/maguro/4.2/ $DEPLOY_BASE/cm10.1/       linaro
 $DO_BUILD tf101  $ANDROID_BASE/tf101/4.1/  $DEPLOY_BASE/tf101/cm10/   standard linaro
 $DO_BUILD tf101  $ANDROID_BASE/tf101/4.2/  $DEPLOY_BASE/tf101/cm10.1/ standard linaro
-
-
-
